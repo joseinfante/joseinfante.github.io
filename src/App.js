25ddcc4c9e9d79e005984357073c8c545b793b19
joseinfante <angel.inf97@gmail.com>
2024-01-09 12:42:09 -0600

    First commit for repo update

--- src/App.js
+++ src/App.js
@@ -3,22 +3,49 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <body>
+    <div id="content">
+        <div id="navigation-bar">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about-me">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#experiance">Experience</a></li>
+                
+              </ul>
+        </div>
+        <h1 id="home">JOSE A. INFANTE</h1>
+        <div id="intro">
+            
+        <div id="about-me">
+            <h1 >hi!</h1>
+            <p>My name is Jose Angel Infante and I recently graduated this Fall 2023, with a Bachelor of Science in Computer Science at the University of Texas at Rio Grande Valley✌️.</p>
+            <p>I am passionate about a wide spectrum of technology, spanning from full-stack development to artificial intelligence and human-computer interactions. My interests encompass the entire range of technologies that bridge the gap between software development and the interaction between humans and machines.</p>
+            <div class="email-me-button">
+                <a href="mailto:angel.infante@angelinfante.com" class="intro-contact">
+                    <button class="email-me">
+                       <ul>
+                            {/* <li><img height="25px" width="25px" src="/resources/icons/email-white.png" alt="Send me an email!"></li> */}
+                            <li>Say hi!</li>
+                       </ul>
+                    </button>
+                </a>
+            </div>
+        </div>
+        </div>
+        <div id="experience">
+            <h1 >-Experience</h1>
+            <p>Here are some technologies I have been working with:</p>
+        </div>
+        {/* end of content div */}
+        </div> 
+        <div id="footer">
+            <div class="ending-credits">
+            <div>Built and designed by Jose A. Infante. </div>
+            <div>All rights reserved. ©</div>
+        </div>
     </div>
+</body>
   );
 }
 
